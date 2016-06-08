818127d8118d441557085f3c2dd6f6fe24221552
KC <KC@Emran-MacBook-Air.local>
2016-06-08 11:05:41 -0600

    another BnB

---
+++ script.js
@@ -0,0 +1,30 @@
+
+var button = document.getElementById('output')
+  button.addEventListener('click', function() {
+var result = document.getElementById('input')
+
+})
+
+var title = document.getElementById('center_column')
+
+var request = new XMLHttpRequest()
+
+request.open("GET", "http://www.omdbapi.com/?s=minions")
+request.send()
+request.onreadystatechange = function(){
+  if(request.readyState === 4 && request.status < 400){
+    var response  = JSON.parse(request.responseText)
+    var array = response.Search
+//console.log(array);
+
+    for (var i = 0; i < array.length; i++) {
+      // create an img element
+      // set the src of that img element to array[i].Poster
+      // append img to parent div
+      var img = document.createElement("img")
+      img.src = array[i].Poster
+      img.innerHTML = array[i].Poster
+      title.appendChild(img)
+      }
+    }
+  }
